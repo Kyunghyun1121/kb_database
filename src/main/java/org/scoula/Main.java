5f0f37e3069fe9000a2fbcaa18460d0184049d0c
Kyumg Hyun <jake.roh2000@gmail.com>
2025-05-21 14:50:20 +0900

    sql jdbc 세팅

--- src/main/java/org/scoula/Main.java
+++ src/main/java/org/scoula/Main.java
@@ -9,5 +9,6 @@ public class Main {
     // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
     System.out.printf("Hello and welcome!");
 
+
   }
 }
---
+++ src/main/java/org/scoula/common/JDBCUtil.java
@@ -0,0 +1,45 @@
+package org.scoula.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class JDBCUtil {
+
+  private static Connection conn = null;
+  // 프로그램 실행 시 자동으로 실행되는 정적 초기화 블록
+  static {
+    try {
+
+      Properties properties = new Properties();
+      properties.load(JDBCUtil.class.getResourceAsStream("/application.properties"));
+      String driver = properties.getProperty("driver");
+      String url = properties.getProperty("url");
+      String id = properties.getProperty("id");
+      String password = properties.getProperty("password");
+
+      Class.forName(driver);
+      conn = DriverManager.getConnection(url, id, password);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static Connection getConnection() {
+    return conn;
+  }
+
+  public static void close() {
+    try {
+      if (conn != null) {
+        conn.close();
+        conn = null;
+      }
+    } catch (
+        SQLException e) {
+      e.printStackTrace();
+    }
+  }
+}
